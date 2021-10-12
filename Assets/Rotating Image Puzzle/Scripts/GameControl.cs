656c186e0bef234c14afe5c9d3bbad2732385578
Adam <46858866+weenie-walker-games@users.noreply.github.com>
2021-10-11 21:16:44 -0400

    Created a picture rotation puzzle based on a YouTube tutorial.

---
+++ Assets/Rotating Image Puzzle/Scripts/GameControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace ImageRotate
+{
+    public class GameControl : MonoBehaviour
+    {
+        /// <summary>
+        /// Based on the YouTube tutorial by Alexander Zotov (https://www.youtube.com/watch?v=TMQrO3Hy_LE)
+        /// Adjusted by me to use more advanced techniques and prevent the need for dragging each picture into the Inspector
+        /// </summary>
+        [SerializeField] private GameObject pictureParent;
+        private List<TouchRotate> pictures = new List<TouchRotate>();
+
+        [SerializeField] private GameObject winText;
+
+        public static bool youWin;
+
+        private void Start()
+        {
+            winText.SetActive(false);
+            youWin = false;
+
+            pictures = pictureParent.GetComponentsInChildren<TouchRotate>().ToList<TouchRotate>();
+        }
+
+        private void Update()
+        {
+            List<TouchRotate> results = pictures.Where(t => t.gameObject.transform.rotation.z != 0).Select(t => t).ToList<TouchRotate>();
+            if (results.Count == 0)
+                youWin = true;
+
+            if (youWin == true)
+                winText.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Rotating Image Puzzle/Scripts/TouchRotate.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ImageRotate
+{
+    public class TouchRotate : MonoBehaviour
+    {
+        private void Start()
+        {
+            int random = Random.Range(0, 4);
+            SetRotation(random);
+        }
+
+        private void OnMouseDown()
+        {
+            if (!GameControl.youWin)
+                transform.Rotate(0, 0f, 90f);
+
+            //Force the rotation to match 0 degrees
+            if(transform.rotation.z < 0.01f && transform.rotation.z > -0.01f)
+            {
+                SetRotation(0, true);
+            }
+        }
+
+
+        private void SetRotation(int random, bool reset = false)
+        {
+            Vector3 rot = Vector3.zero;
+            if (reset)
+                rot.z = 0;
+            else
+                rot.z += 90 * random;
+            transform.rotation = Quaternion.Euler(rot);
+        }
+
+    }
+}
